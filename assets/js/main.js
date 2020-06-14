f34586883e94c6d5f07f53d0ff0bd704522606c7
Derbeca <derbeca@hotmail.com>
2020-06-14 18:08:53 +0200

    ajout js boutons

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,13 +11,32 @@ var btnInterUrbain = document.getElementById('btnInterUrbain');
 var btnVoyage = document.getElementById('btnVoyage');
 var btnSante = document.getElementById('btnSante');
 var btnMarketplace = document.getElementById('btnMarketplace');
-var btnFermer = document.getElementsByTagName('btnFermer');
+
+var btnFermer = document.getElementsByClassName('btnFermer');
 
 
-// montrer modal inter-urbain
+
+// montrer modals
 btnInterUrbain.addEventListener('click', function() {
     modalInterUrbain.style.display = 'block';
 });
-btnFermer.addEventListener('click', function() {
-    modalInterUrbain.style.display = "none";
+btnVoyage.addEventListener('click', function() {
+    modalVoyage.style.display = 'block';
 });
+btnSante.addEventListener('click', function() {
+    modalSante.style.display = 'block';
+});
+btnMarketplace.addEventListener('click', function() {
+    modalMarketplace.style.display = 'block';
+});
+
+// fermer modal
+for(let i = 0; i < btnFermer.length; i++) {
+    var modal = document.querySelectorAll('.modal');
+    btnFermer[i].addEventListener('click', function() {
+        console.log('tu as clické');
+        for(let j = 0; j < modal.length; j++) {
+            modal[j].style.display = 'none';
+        }
+    })
+  }
